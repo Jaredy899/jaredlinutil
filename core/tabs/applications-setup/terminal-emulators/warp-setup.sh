f754159dd7d26e885c7b085b088b38afd8ab81cf
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2025-02-08 13:52:17 -0500

    try this

--- core/tabs/applications-setup/terminal-emulators/warp-setup.sh
+++ core/tabs/applications-setup/terminal-emulators/warp-setup.sh
@@ -66,11 +66,14 @@ installWarp() {
                 # Add AppImage to system apps
                 if [ -x "$appimage_path" ]; then
                     mkdir -p "$HOME/.local/share/applications"
+                    mkdir -p "$HOME/.local/share/icons"
+                    curl -o "$HOME/.local/share/icons/warp.png" "https://www.warp.dev/static/image/r/w=3840,q=90,format=auto/header_cf707f3073.png"
+                    ICON_PATH="$HOME/.local/share/icons/warp.png"
                     cat <<EOF > "$HOME/.local/share/applications/warp.desktop"
 [Desktop Entry]
 Name=Warp
 Exec=$appimage_path
-Icon=utilities-terminal
+Icon=$ICON_PATH
 Type=Application
 Categories=Utility;
 EOF
