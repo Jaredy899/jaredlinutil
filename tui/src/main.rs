7ab23f12702e769fd6ffca1969152c6a0488452a
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2025-02-09 16:47:16 -0500

    refactor

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,4 +1,3 @@
-mod cli;
 mod confirmation;
 mod filter;
 mod float;
@@ -12,7 +11,7 @@ mod theme;
 #[cfg(feature = "tips")]
 mod tips;
 
-use crate::cli::Args;
+use crate::theme::Theme;
 use clap::Parser;
 use ratatui::{
     backend::CrosstermBackend,
@@ -24,20 +23,45 @@ use ratatui::{
     },
     Terminal,
 };
+use running_command::TERMINAL_UPDATED;
 use state::AppState;
 use std::{
     io::{stdout, Result, Stdout},
+    path::PathBuf,
+    sync::atomic::Ordering,
     time::Duration,
 };
 
+// Linux utility toolbox
+#[derive(Debug, Parser)]
+pub struct Args {
+    #[arg(short, long, help = "Path to the configuration file")]
+    config: Option<PathBuf>,
+    #[arg(short, long, value_enum)]
+    #[arg(default_value_t = Theme::Default)]
+    #[arg(help = "Set the theme to use in the application")]
+    theme: Theme,
+    #[arg(
+        short = 'y',
+        long,
+        help = "Skip confirmation prompt before executing commands"
+    )]
+    skip_confirmation: bool,
+    #[arg(long, default_value_t = false)]
+    #[clap(help = "Show all available options, disregarding compatibility checks (UNSAFE)")]
+    override_validation: bool,
+    #[arg(long, default_value_t = false)]
+    #[clap(help = "Bypass the terminal size limit")]
+    size_bypass: bool,
+}
+
 fn main() -> Result<()> {
     let args = Args::parse();
-    let mut state = AppState::new(args.clone());
+
+    let mut state = AppState::new(args);
 
     stdout().execute(EnterAlternateScreen)?;
-    if args.mouse {
-        stdout().execute(EnableMouseCapture)?;
-    }
+    stdout().execute(EnableMouseCapture)?;
 
     enable_raw_mode()?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
@@ -48,9 +72,7 @@ fn main() -> Result<()> {
     // restore terminal
     disable_raw_mode()?;
     terminal.backend_mut().execute(LeaveAlternateScreen)?;
-    if args.mouse {
-        terminal.backend_mut().execute(DisableMouseCapture)?;
-    }
+    terminal.backend_mut().execute(DisableMouseCapture)?;
     terminal.backend_mut().execute(ResetColor)?;
     terminal.show_cursor()?;
 
@@ -59,9 +81,14 @@ fn main() -> Result<()> {
 
 fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>, state: &mut AppState) -> Result<()> {
     loop {
-        terminal.draw(|frame| state.draw(frame)).unwrap();
         // Wait for an event
         if !event::poll(Duration::from_millis(10))? {
+            if TERMINAL_UPDATED
+                .compare_exchange(true, false, Ordering::AcqRel, Ordering::Acquire)
+                .is_ok()
+            {
+                terminal.draw(|frame| state.draw(frame)).unwrap();
+            }
             continue;
         }
 
@@ -84,5 +111,6 @@ fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>, state: &mut AppState) 
             }
             _ => {}
         }
+        terminal.draw(|frame| state.draw(frame)).unwrap();
     }
 }
