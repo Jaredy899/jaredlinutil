3ec8d15fceca46c725784eac216ff4ad0bf694d0
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2025-02-15 16:01:11 -0500

    apt depends

--- core/tabs/system-setup/compile-setup.sh
+++ core/tabs/system-setup/compile-setup.sh
@@ -22,7 +22,7 @@ installDepend() {
             "$ESCALATION_TOOL" "$PACKAGER" update
             "$ESCALATION_TOOL" dpkg --add-architecture i386
             "$ESCALATION_TOOL" "$PACKAGER" update
-            "$ESCALATION_TOOL" "$PACKAGER" install -y "$DEPENDENCIES" "$COMPILEDEPS"
+            "$ESCALATION_TOOL" "$PACKAGER" install -y $DEPENDENCIES "$COMPILEDEPS"
             ;;
         dnf)
             "$ESCALATION_TOOL" "$PACKAGER" update -y
