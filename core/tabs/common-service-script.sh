dd4c39d6af811504c402b73a409f2abbe2c0d72f
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2025-02-21 19:05:04 -0500

    fix?

--- core/tabs/common-service-script.sh
+++ core/tabs/common-service-script.sh
@@ -2,7 +2,7 @@
 
 checkInitManager() {
     for manager in $1; do
-        if command_exists "$manager" || command_exists "$ESCALATION_TOOL $manager"; then
+        if command_exists "$manager" || { $ESCALATION_TOOL command_exists "$manager"; }; then
             INIT_MANAGER="$manager"
             printf "%b\n" "${CYAN}Using ${manager} to interact with init system${RC}"
             break
