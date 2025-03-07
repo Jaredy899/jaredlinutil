c51ac68e2deb5eb9a1dbb767720753595750c967
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2025-03-06 19:02:35 -0500

    rpm

--- core/tabs/system-setup/fedora/rpm-fusion-setup.sh
+++ core/tabs/system-setup/fedora/rpm-fusion-setup.sh
@@ -11,12 +11,10 @@ installRPMFusion() {
                 printf "%b\n" "${YELLOW}Installing RPM Fusion...${RC}"
                 "$ESCALATION_TOOL" "$PACKAGER" install "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora)".noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
                 if "$PACKAGER" --version | grep -q "5."; then
-                    printf "%b\n" "${YELLOW}Using DNF5 syntax for enabling repositories...${RC}"
                     "$ESCALATION_TOOL" "$PACKAGER" config-manager setopt --repo=fedora-cisco-openh264 enabled=1
                     "$ESCALATION_TOOL" "$PACKAGER" config-manager setopt --repo=rpmfusion-nonfree-updates enabled=1
                     "$ESCALATION_TOOL" "$PACKAGER" config-manager setopt --repo=rpmfusion-free-updates enabled=1
                 else
-                    printf "%b\n" "${YELLOW}Using older DNF syntax for enabling repositories...${RC}"
                     "$ESCALATION_TOOL" "$PACKAGER" config-manager --set-enabled fedora-cisco-openh264
                     "$ESCALATION_TOOL" "$PACKAGER" config-manager --set-enabled rpmfusion-nonfree-updates
                     "$ESCALATION_TOOL" "$PACKAGER" config-manager --set-enabled rpmfusion-free-updates
