21be08bd51c4dc8fbe298997a3be9170fdb8903d
Jared Cervantes <jared@jaredcervantes.com>
2025-04-25 13:02:12 -0400

    fix fedora gaming

--- core/tabs/system-setup/gaming-setup.sh
+++ core/tabs/system-setup/gaming-setup.sh
@@ -40,13 +40,13 @@ installDepend() {
             "$ESCALATION_TOOL" "$PACKAGER" install -y $DEPENDENCIES $DISTRO_DEPS
             ;;
         dnf)
-            if [ "$(rpm -E %fedora)" -le 41 ]; then
-                "$ESCALATION_TOOL" "$PACKAGER" install ffmpeg ffmpeg-libs -y
-                "$ESCALATION_TOOL" "$PACKAGER" install -y $DEPENDENCIES
-            else
-                printf "%b\n" "${CYAN}Fedora < 41 detected. Installing rpmfusion repos.${RC}"
+            if [ "$(rpm -E %fedora)" -gt 41 ]; then
+                printf "%b\n" "${CYAN}Fedora > 41 detected. Installing rpmfusion repos.${RC}"
                 "$ESCALATION_TOOL" "$PACKAGER" install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm -y
                 "$ESCALATION_TOOL" "$PACKAGER" config-manager setopt --repo fedora-cisco-openh264 enabled=1
+                "$ESCALATION_TOOL" "$PACKAGER" install -y $DEPENDENCIES
+            else
+                "$ESCALATION_TOOL" "$PACKAGER" install ffmpeg ffmpeg-libs -y
                 "$ESCALATION_TOOL" "$PACKAGER" install -y $DEPENDENCIES
             fi
             ;;
