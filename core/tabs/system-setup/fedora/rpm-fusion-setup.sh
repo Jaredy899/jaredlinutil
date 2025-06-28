535c66d9172b6f3d0d1a8efff2fb9c6f74099b39
Jared Cervantes <jared@jaredcervantes.com>
2025-06-28 13:09:52 -0400

    fix rpm

--- core/tabs/system-setup/fedora/rpm-fusion-setup.sh
+++ core/tabs/system-setup/fedora/rpm-fusion-setup.sh
@@ -9,16 +9,20 @@ installRPMFusion() {
         dnf)
             if [ ! -e /etc/yum.repos.d/rpmfusion-free.repo ] || [ ! -e /etc/yum.repos.d/rpmfusion-nonfree.repo ]; then
                 printf "%b\n" "${YELLOW}Installing RPM Fusion...${RC}"
-                "$ESCALATION_TOOL" "$PACKAGER" install "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora)".noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
-                if "$PACKAGER" --version | grep -q "5."; then
-                    "$ESCALATION_TOOL" "$PACKAGER" config-manager setopt --repo=fedora-cisco-openh264 enabled=1
-                    "$ESCALATION_TOOL" "$PACKAGER" config-manager setopt --repo=rpmfusion-nonfree-updates enabled=1
-                    "$ESCALATION_TOOL" "$PACKAGER" config-manager setopt --repo=rpmfusion-free-updates enabled=1
+                
+                "$ESCALATION_TOOL" "$PACKAGER" install -y \
+                    "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+                    "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+
+                fedora_version=$(rpm -E %fedora)
+                if [ "$fedora_version" -ge 41 ]; then
+                    "$ESCALATION_TOOL" "$PACKAGER" config-manager setopt fedora-cisco-openh264.enabled=1
                 else
-                    "$ESCALATION_TOOL" "$PACKAGER" config-manager --set-enabled fedora-cisco-openh264
-                    "$ESCALATION_TOOL" "$PACKAGER" config-manager --set-enabled rpmfusion-nonfree-updates
-                    "$ESCALATION_TOOL" "$PACKAGER" config-manager --set-enabled rpmfusion-free-updates
+                    "$ESCALATION_TOOL" "$PACKAGER" config-manager --enable fedora-cisco-openh264
                 fi
+                
+                "$ESCALATION_TOOL" "$PACKAGER" install -y rpmfusion-\*-appstream-data
+                
                 printf "%b\n" "${GREEN}RPM Fusion installed and enabled${RC}"
             else
                 printf "%b\n" "${GREEN}RPM Fusion already installed${RC}"
