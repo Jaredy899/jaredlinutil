cebd1a5fd7f047c59fc0afb8e61884cbd68c24ed
Jared Cervantes <jared@jaredcervantes.com>
2025-06-28 13:36:03 -0400

    fix rpm with tainted

--- core/tabs/system-setup/fedora/rpm-fusion-setup.sh
+++ core/tabs/system-setup/fedora/rpm-fusion-setup.sh
@@ -13,7 +13,7 @@ installRPMFusion() {
                 "$ESCALATION_TOOL" "$PACKAGER" install -y \
                     "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
                     "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
-
+                
                 fedora_version=$(rpm -E %fedora)
                 if [ "$fedora_version" -ge 41 ]; then
                     "$ESCALATION_TOOL" "$PACKAGER" config-manager setopt fedora-cisco-openh264.enabled=1
@@ -23,7 +23,27 @@ installRPMFusion() {
                 
                 "$ESCALATION_TOOL" "$PACKAGER" install -y rpmfusion-\*-appstream-data
                 
-                printf "%b\n" "${GREEN}RPM Fusion installed and enabled${RC}"
+                printf "%b\n" "${YELLOW}Do you want to install tainted repositories? [y/N]: ${RC}"
+                read -r install_tainted
+                case "$install_tainted" in
+                    [Yy]*)
+                        printf "%b\n" "${YELLOW}Installing RPM Fusion tainted repositories...${RC}"
+                        "$ESCALATION_TOOL" "$PACKAGER" install -y rpmfusion-free-release-tainted rpmfusion-nonfree-release-tainted
+                        
+                        if [ "$fedora_version" -ge 41 ]; then
+                            "$ESCALATION_TOOL" "$PACKAGER" config-manager setopt rpmfusion-free-tainted.enabled=1
+                            "$ESCALATION_TOOL" "$PACKAGER" config-manager setopt rpmfusion-nonfree-tainted.enabled=1
+                        else
+                            "$ESCALATION_TOOL" "$PACKAGER" config-manager --set-enabled rpmfusion-free-tainted
+                            "$ESCALATION_TOOL" "$PACKAGER" config-manager --set-enabled rpmfusion-nonfree-tainted
+                        fi
+                        printf "%b\n" "${GREEN}RPM Fusion (including tainted repositories) installed and enabled${RC}"
+                        ;;
+                    *)
+                        printf "%b\n" "${BLUE}Skipping tainted repositories${RC}"
+                        printf "%b\n" "${GREEN}RPM Fusion installed and enabled${RC}"
+                        ;;
+                esac
             else
                 printf "%b\n" "${GREEN}RPM Fusion already installed${RC}"
             fi
