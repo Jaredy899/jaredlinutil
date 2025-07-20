c40ec75bbee58d7a1d045a86c56839d27de7afb2
Jared Cervantes <jared@jaredcervantes.com>
2025-07-20 15:33:08 -0400

    tailcale solus

--- core/tabs/security/tailscale.sh
+++ core/tabs/security/tailscale.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 
 . ../common-script.sh
+. ../common-service-script.sh
 
 installTailscale() {
     if ! command_exists tailscale; then
@@ -8,6 +9,10 @@ installTailscale() {
         case "$PACKAGER" in
             eopkg)
                 "$ESCALATION_TOOL" "$PACKAGER" install -y tailscale
+                startAndEnableService tailscaled
+                printf "%b\n" "${GREEN}Tailscale installed successfully!${RC}"
+                printf "%b\n" "${YELLOW}Please run 'sudo tailscale up' to complete the setup${RC}"
+                return
                 ;;
             *)
                 curl -fsSL https://tailscale.com/install.sh | sh
@@ -19,6 +24,9 @@ installTailscale() {
 }
 
 configureTailscale() {
+    if [ "$PACKAGER" = "eopkg" ]; then
+        return
+    fi
     printf "%b\n" "${YELLOW}Configuring Tailscale...${RC}"
     "$ESCALATION_TOOL" tailscale up
 }
